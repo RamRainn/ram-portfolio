4b3d495bb828725d3293e667f9dbe792e3478762
RamRain <ramrain.411@gmail.com>
2022-09-22 15:16:52 +0500

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  important: true,
+  theme: {
+    extend: {},
+    fontFamily: {
+      'roboto': ['Roboto Condensed'],
+    },
+    colors: {
+      main: '#12243d',
+      primary: '#56f5cf',
+      white: '#ffffff'
+    }
+  },
+  plugins: [],
+}
